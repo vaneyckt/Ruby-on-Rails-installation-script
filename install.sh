cfd9f37a9f1b613926d5416a44a6fe5660eefbda
Tom Van Eyck <tom.vaneyck@swrve.com>
2013-04-28 20:06:51 +0200

    make rvm enable autolibs

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ sudo apt-get install build-essential openssl libreadline6 libreadline6-dev \
 curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-dev sqlite3 \
 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison  \
 subversion &&
+rvm autolibs enable &&
 rvm install 1.9.3 &&
 rvm --default use 1.9.3 &&
 gem install rails &&
